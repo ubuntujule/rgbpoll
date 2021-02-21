3706e4911e18e7568d033b93cab08bf1554dd596
Grimm <ubuntujule@gmail.com>
2021-02-21 16:08:48 +0100

    table with input for each appointment

--- poll/views.py
+++ poll/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from prettytable import PrettyTable
 from django.template import loader
 
 def index(request):
     template = loader.get_template('poll/index.html')
-    x = PrettyTable()
-    x.field_names= ["member", "boot"]
-    context = {'test': 'Test test Test'}
+    #x = PrettyTable()
+    #x.field_names= ["member", "boot"]
+    context = {}
+    context['header'] = 'RGB PLaner'
+    context['theader'] = ['WER?', 'Datum', 'Von', 'Bis', 'Notizen']
+    print(request.GET.get("date")) 
     return HttpResponse(template.render(context, request))
-#def index(request):
-#    return HttpResponse("Hello, world. You're at the polls index.")
+    
