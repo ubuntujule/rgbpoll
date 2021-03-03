f51ea4b015bc299a28e304c1242963e2e8dfa233
Grimm <ubuntujule@gmail.com>
2021-03-03 22:37:29 +0100

    add api with dockerfile including gunicorn
    probably run with "gunicorn api:app -b 0:80
    but actually don't know why there are ports 80:8000

--- api/api.py
+++ api/api.py
@@ -1,3 +1,44 @@
 import falcon
+import json
+
+"""
+ # list comprehension for later
+ # [{'entry':entry, '_self':f'/report/{entry}', 'data':data} for entry, data in db.items()]
+
+"""
+
+class Entries():
+    def on_get(self, req, resp):
+        resp.media = db
+
+
+class Entry():
+    def on_put(self, req, resp, _id):
+        data = json.load(req.stream)
+
+        print(id)
+        db[id] = data 
+        write_on_json()
+        resp.media = {'id': _id, '_self':f'/entry/{_id}', 'data':data} # instead of data db[_id]
+
+
+def write_on_json():
+
+    with open('data.json', 'w') as f:
+        json.dump(db, f)
+try:
+    with open(data.json) as f:
+        db = json.load(f)
+except (FileNotFoundError):
+    db = {}
+
+# from falcon_cors import CORS
+# cors = CORS(allow_all_origins=True, allow_methods_list=['GET'])
+
+# app = falcon.API(middleware=[cors.middleware])
 
 app = falcon.API()
+app.add_route('/', Entries())
+
+
+# gunicorn name:app -b 0:80 --reload
